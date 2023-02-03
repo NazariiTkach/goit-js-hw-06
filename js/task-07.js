bb2cdd4c6c2b9d909ca82c893a2de401fa53a6ab
Nazarii Tkach <113298067+NazariiTkach@users.noreply.github.com>
2023-02-03 19:32:06 +0100

    corect

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const inputChange = document.querySelector('#font-size-control')
 const textSpan = document.querySelector('#text')
 
-inputChange.addEventListener('change', event => {
+inputChange.addEventListener('input', event => {
     text.style.fontSize = `${event.target.value}px`
 });
 
